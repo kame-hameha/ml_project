095aa4498b380d0c56dc44c7ee6e9138e41b7018
Kai Metzger <kai.metzger@icloud.com>
2025-04-05 09:38:31 +0200

    Create install-raspi.sh
    Create bash-skript to automatically install software.

---
+++ installation/install-raspi.sh
@@ -0,0 +1,33 @@
+
+#!/bin/bash
+
+# 1. Update und Upgrade
+echo "=== System wird aktualisiert ==="
+sudo apt update && sudo apt upgrade -y
+
+# Notwendige Pakete und Visual Studio Code installieren
+sudo apt install -y apt-transport-https code
+
+# 2. Git-Repository herunterladen ins Home-Verzeichnis
+echo "=== ml_project Repo herunterladen ==="
+cd ~
+git clone https://github.com/kame-hameha/ml_project
+cd ml_project
+
+# 3. Virtuelle Umgebung erstellen und aktivieren
+echo "=== Virtuelle Umgebung wird erstellt ==="
+python3 -m venv .venv
+source .venv/bin/activate
+
+# 4. Benötigte Bibliotheken installieren
+echo "=== Python-Bibliotheken werden installiert ==="
+pip install --upgrade pip
+pip install ipykernel
+python3 -m ipykernel install --user --name=ml_project_kernel
+
+# 5. Weitere Pakete installieren (auch innerhalb Jupyter nutzbar)
+pip install opencv-python tensorflow matplotlib pandas scipy jupyter
+
+echo "=== Installation abgeschlossen ==="
+echo "Starte Jupyter Notebook mit: jupyter notebook oder in Visual Studio Code"
+source .venv/bin/activate
